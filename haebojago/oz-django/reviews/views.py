6f22498246aaddc4f73611548676fff1cce8b236
benchprincess <155422991+benchprincess@users.noreply.github.com>
2024-02-19 02:19:44 +0900

    Add files via upload

---
+++ haebojago/oz-django/reviews/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Review
+from .serializers import ReviewSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
+
+# Create your views here.
+
+# api/v1/reviews [GET]
+class Reviews(APIView):
+    def get(self, request):
+        reviews = Review.objects.all()
+
+        serializer = ReviewSerializer(reviews, many=True)
+
+        return Response(serializer.data)
+
+# api/v1/review_id [GET]
+class ReviewDetail(APIView):
+    def get(self, request, review_id):
+        try:
+           review = Review.objects.get(id=review_id)
+        except:
+            raise NotFound
+        
+        serializer = ReviewSerializer(review)
+
+        return Response(serializer.data)
